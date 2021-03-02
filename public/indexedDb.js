429134da0a05304f1be12f8f374636234778d026
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-02 15:24:10 -0800

    Send pending if more than zero

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -33,3 +33,32 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
     store.add(record);
 };
+
+// Send pending if more than zero
+function getPending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            }
+            })
+            .then(response => {
+            return response.json();
+            })
+            .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+            });
+        }
+    };
+}
