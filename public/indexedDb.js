37757837d1b38cab7f1c34429653c3827cc115e7
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-02 12:15:17 -0800

    included prefixes of indexedDb implementations for more browswer compatibility

---
+++ public/indexedDb.js
@@ -0,0 +1,5 @@
+// test code in browsers that still use a prefix
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// This line should only be needed if it is needed to support the object's constants for older browsers
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; 
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
