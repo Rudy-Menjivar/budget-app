f1751b17cabc0f5434713e4d1da30f74b2508ed5
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-02 12:16:28 -0800

    if stmt will log error when browswer isn't indexedDb compatible

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -3,3 +3,7 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 // This line should only be needed if it is needed to support the object's constants for older browsers
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; 
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+if (!window.indexedDB) {
+    console.log("Your browser doesn't support a stable version of IndexedDB.");
+};
